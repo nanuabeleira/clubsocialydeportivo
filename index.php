710f38b284d5fef8ca76cca7bc0726c1785d9413
nanuabeleira <nanuabeleira@gmail.com>
2019-11-06 21:17:28 -0300

    Actualizaciones en clase

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Club Social y Deportivo XXXXX </title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+
+  </head>
+  <body>
+    <h1>Bienvenido a la página del Club</h1>
+    <p>Web para clubs deportivos y red social para sus miembros.
+Objetivo: Poder ofrecer a los clubes de Mendoza, luego el mundo una página web
+	1er meta: Registro de usuarios
+	2da meta: Pagina de noticias
+	3er meta: Que los socios / usuarios registrados puedan interactuar con las notas publicadas
+	4ta meta: Poder gestionar la base de datos de los socios/atletas para análisis de datos. Que profesores de diferentes
+		deportes pueda ir cargando los resultados del atleta en la base de datos
+	5ta meta: Poder cobrar cuota social por la página
+</p>
+  </body>
+</html>
