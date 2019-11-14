ce794071dcd485027e5b0a07646811803815c845
nanuabeleira <nanuabeleira@gmail.com>
2019-11-14 19:28:44 -0300

    no sabría si conservar estos últimos cambios

--- header.php
+++ header.php
@@ -7,29 +7,21 @@
     <link rel="icon" type="image/png" href="images/logo.png" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-<!Home - Con información general del proyecto
-F.A.Q. - Preguntas frecuentes
-Formulario de Registro - Visual, pero no funcional
-Formulario de Login - Visual, pero no funcional
-Formulario de Contacto - Visual, pero no funcional
-Perfil de Usuario>
   </head>
-
-
-  <body>
+<body>
 <header>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="index.php">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">F.A.Q.</a>
+        <a class="nav-link" href="preguntasyrespuestas.php">F.A.Q.</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href=registro/regform.php>Formulario de Registro</a>
--- index.php
+++ index.php
@@ -3,9 +3,9 @@
   <head>
     <meta charset="utf-8">
     <title>Club Xxx Nnn</title>
-    <link rel="stylesheet" href="css/styles.css">
     <link rel="icon" type="image/png" href="images/logo.png" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
 
 <!Home - Con información general del proyecto
 F.A.Q. - Preguntas frecuentes
@@ -22,13 +22,12 @@ include_once ("header.php");
  ?>
     <h1>Bienvenido a la página del Club</h1>
     <p>Web para clubs deportivos y red social para sus miembros.
-Objetivo: Poder ofrecer a los clubes de Mendoza, luego el mundo una página web
-	1er meta: Registro de usuarios
-	2da meta: Pagina de noticias
-	3er meta: Que los socios / usuarios registrados puedan interactuar con las notas publicadas
-	4ta meta: Poder gestionar la base de datos de los socios/atletas para análisis de datos. Que profesores de diferentes
-		deportes pueda ir cargando los resultados del atleta en la base de datos
-	5ta meta: Poder cobrar cuota social por la página
+Objetivo: Poder ofrecer a los clubes de Mendoza, luego el mundo una página web<br>
+	<p>1er meta: Registro de usuarios </p>
+	<p>2da meta: Pagina de noticias</p>
+	<p>3er meta: Que los socios / usuarios registrados puedan interactuar con las notas publicadas</p>
+	<p>4ta meta: Poder gestionar la base de datos de los socios/atletas para análisis de datos. Que profesores de diferentesdeportes pueda ir cargando los resultados del atleta en la base de datos</p>
+	<p>5ta meta: Poder cobrar cuota social por la página</p>
 </p>
 
 <footer>
