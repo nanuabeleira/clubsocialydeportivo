45e70f549b7391d1c6cb14d41e2f277ddd27d34e
nanuabeleira <nanuabeleira@gmail.com>
2019-11-14 14:56:08 -0300

    Estuve trabajando en el index. Barra de Navegacion. CSS. Por favor sientanse libres de trabajar y modificar

--- index.php
+++ index.php
@@ -2,14 +2,49 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Riestra</title>
-    <link rel="icon" type="image/png" href="images/logoriestra.png" />
-
+    <title>Club Xxx Nnn</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="icon" type="image/png" href="images/logo.png" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
 
+<!Home - Con información general del proyecto
+F.A.Q. - Preguntas frecuentes
+Formulario de Registro - Visual, pero no funcional
+Formulario de Login - Visual, pero no funcional
+Formulario de Contacto - Visual, pero no funcional
+Perfil de Usuario>
   </head>
+
+
   <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">F.A.Q.</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Formulario de Registro</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Login</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contacto</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Perfil del usuario</a>
+      </li>
+    </ul>
+  </div>
+</nav>
     <h1>Bienvenido a la página del Club</h1>
     <p>Web para clubs deportivos y red social para sus miembros.
 Objetivo: Poder ofrecer a los clubes de Mendoza, luego el mundo una página web
